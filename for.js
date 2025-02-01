5712c4bef84f632080d6dc3c5696800e56e0e68f
Worawat Kaewsanmueng <lottett@MacBook-Air-khxng-Worawat.local>
2025-02-01 19:23:31 +0700

    Task 6.8 ตรวจสอบชื่อ

--- for.js
+++ for.js
@@ -1,5 +1,9 @@
-let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
+let names = ['Pusit', 'Worawat', 'Film', 'Tide', 'Kornkanok'];
 
-for (let i = 0; i < color.length; i++) {
-    console.log(color[i]);
+for (let i = 0; i < names.length; i++) {
+    if (names[i].length >= 6) {
+        console.log('สวัสดีคุณ ' + names[i]);
+    } else {
+        console.log('Hello ' + names[i]);
+    }
 }
