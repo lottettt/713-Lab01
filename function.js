cd828ff1a9079cb5976a744e5ada896d6c33efa1
Worawat Kaewsanmueng <lottett@MacBook-Air-khxng-Worawat.local>
2025-02-03 21:10:06 +0700

    Task 8.4

--- function.js
+++ function.js
@@ -1,5 +1,11 @@
-function greet(name) {
-    return 'Hello ' + name;
+x = [12, 30, 15, 50]
+
+function sum_array(x) {
+    var sum = 0;
+    for (var i = 0; i < x.length; i++) {
+        sum += x[i];
+    }
+    return sum;
 }
 
-console.log(greet('Pusit'));
+console.log(sum_array(x));
