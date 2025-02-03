c44ad13d59562c0769a69aa5f3a9400012a07a43
Worawat Kaewsanmueng <lottett@MacBook-Air-khxng-Worawat.local>
2025-02-03 21:47:16 +0700

    Task 9.1 Function Expression

--- function.js
+++ function.js
@@ -1,11 +1,5 @@
-x = [12, 30, 15, 50]
-
-function sum_array(x) {
-    var sum = 0;
-    for (var i = 0; i < x.length; i++) {
-        sum += x[i];
-    }
-    return sum;
+const greet = function greet(name) {
+    return 'Hello ' + name;
 }
 
-console.log(sum_array(x));
+console.log(greet('Pusit'));
