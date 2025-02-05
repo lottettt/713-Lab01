3feb409d7d7e37070c56371c1acdfbeaf5c52968
Worawat Kaewsanmueng <lottett@MacBook-Air-khxng-Worawat.local>
2025-02-05 22:07:20 +0700

    Task 11.4

--- json.js
+++ json.js
@@ -4,4 +4,25 @@ const object = {
     city : 'New York'
 }
 
+console.log(object);
 console.log(object.name);
+
+const jsonStr = JSON.stringify(object);
+console.log(jsonStr);
+
+const info_object = {
+    name : 'Henry',
+    surname : 'Fayol',
+    age : 22,
+    grade : 2.4
+}
+
+const retire = (info_object) => {
+    if (info_object.grade < 2.5) {
+        return 'คุณ ' + info_object.name + ' ' + info_object.surname + ' ผ้นสภาพนักศึกษา';
+    } else {
+        return JSON.stringify(info_object);
+    }
+}
+
+console.log(retire(info_object));
